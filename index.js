c36386fb41647494e3371733d37995c5eff682fc
manjulachohan <manjulachohan50@gmail.com>
2024-07-05 19:51:07 +0500

    simplecalculator

---
+++ index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const asnwer = await inquirer.prompt([
+    { massage: "Enter first number", type: "number", name: "firstNumber" },
+    { massage: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        massage: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//corditional statement
+if (asnwer.operator === "Addition") {
+    console.log(asnwer.firstNumber + asnwer.secondNumber);
+}
+else if (asnwer.operator === "Subtraction") {
+    console.log(asnwer.firstNumber - asnwer.secondNumber);
+}
+else if (asnwer.operator === "Multiplication") {
+    console.log(asnwer.firstNumber * asnwer.secondNumber);
+}
+else if (asnwer.operator === "Division") {
+    console.log(asnwer.firstNumber / asnwer.secondNumber);
+}
+else {
+    console.log("Please select valid operator");
+}
